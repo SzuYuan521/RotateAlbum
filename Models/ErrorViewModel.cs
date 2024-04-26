0d8daa015bf02b1eb31f039d6fc66b5a86ef65b6
Bear <SzuYuan521@gmail.com>
2024-04-26 18:43:36 +0800

    [+] 新增專案, 按鈕, 點擊按鈕後播放背景音樂, 正方體六面皆為安妮亞圖片
    1. 新增style.css檔案用以控制正方體的旋轉及圖片
    2. 修改Index.cshtml, 新增#btnID及#albumID, 控制按鈕以及正方體
    3. 修改背景為粉色, 色碼:#ffc0cb
    4. 新增javaScript.js並引用, 用來控制音樂開關以及按鈕、正方體顯示
    5. 刪掉預設檔案的部分設定
    6. 新增img資料夾放入圖片素材
    7. 新增music資料夾放入背景音樂

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RotateAlbum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/javaScript.js
@@ -0,0 +1,12 @@
+﻿function playMusic() {
+    var bgMusic = document.getElementById("bgmusic");
+    var album = document.getElementById("album");
+    var btn = document.getElementById("btn");
+    console.log(btn.style.display);
+    bgMusic.play();
+    album.style.display = "flex";
+    // 移除按鈕點擊事件處理程序以避免多次觸發播放
+    btn.removeEventListener("click", playMusic);
+    btn.style.display = "none"; 
+    console.log(btn.style.display);
+}
